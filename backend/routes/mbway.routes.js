4e7412174fd447f7866add2c1e9777e9a4e0c41e
palash2397 <palashpatil55@gmail.com>
2026-06-05 18:02:48 +0530

    payment status router

--- backend/routes/mbway.routes.js
+++ backend/routes/mbway.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createPaymentIntent } from "../controllers/mbway.controller.js";
+import {
+  createPaymentIntent,
+  getPaymentStatus,
+} from "../controllers/mbway.controller.js";
 
 const mbwayRouter = Router();
 
 mbwayRouter.post("/create-payment", createPaymentIntent);
+mbwayRouter.get("/status/:paymentIntentId", getPaymentStatus);
 
 export default mbwayRouter;
