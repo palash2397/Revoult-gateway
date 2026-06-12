f70424ee7330e56c21192349b1cd8acb756430f9
palash2397 <palashpatil55@gmail.com>
2026-06-12 15:56:03 +0530

    Partial refund route

--- backend/routes/mbway.routes.js
+++ backend/routes/mbway.routes.js
@@ -3,6 +3,7 @@ import {
   createPaymentIntent,
   getPaymentStatus,
   createFullRefund,
+  createPartialRefund,
 } from "../controllers/mbway.controller.js";
 
 const mbwayRouter = Router();
@@ -10,5 +11,6 @@ const mbwayRouter = Router();
 mbwayRouter.post("/create-payment", createPaymentIntent);
 mbwayRouter.get("/status/:paymentIntentId", getPaymentStatus);
 mbwayRouter.post("/full/refund", createFullRefund);
+mbwayRouter.post("/partial/refund", createPartialRefund);
 
 export default mbwayRouter;
